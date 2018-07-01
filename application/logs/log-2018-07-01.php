811a3af19e70a175a12dc3078c135c028b822a31
CandraBlackz <blackz.wd@gmail.com>
2018-07-01 22:48:02 +0700

    mini inventory

---
+++ application/logs/log-2018-07-01.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-07-01 17:38:49 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\Mini-Inventory-and-Sales-Management-System\application\controllers\Administrators.php 55
+ERROR - 2018-07-01 17:38:49 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\Mini-Inventory-and-Sales-Management-System\application\controllers\Administrators.php 71
